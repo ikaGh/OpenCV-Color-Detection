59845cd93e4f95993f43b15c497678e3bc02a7a9
ikaGh <128742286+ikaGh@users.noreply.github.com>
2024-06-09 21:40:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import cv2
+from PIL import Image
+
+from util import get_limits
+
+yellow = [0, 255, 255]  # yellow in BGR colorspace
+
+# Define the video capture object
+cap = cv2.VideoCapture(0)  # 0 indicates the default camera
+
+# Check if the camera is opened successfully
+if not cap.isOpened():
+    print("Error opening camera")
+    exit()
+
+# Capture and display frames until 'q' key is pressed
+while True:
+    ret, frame = cap.read()
+
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lowerLimit, upperLimit = get_limits(color=yellow)
+
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+
+    mask_ = Image.fromarray(mask)
+
+    bbox = mask_.getbbox()
+
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the camera and close all windows
+cap.release()
+cv2.destroyAllWindows()
